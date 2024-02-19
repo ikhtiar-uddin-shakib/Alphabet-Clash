630cedc3c6c6774d99ee446d74e3e55735cba692
Md Ikhtiar Uddin <mdikhtiaruddin786@gmail.com>
2024-02-19 23:00:46 +0600

    js added

---
+++ scripts/alpha.js
@@ -0,0 +1,13 @@
+// function play(){
+//  homeSection = document.getElementById('home-screen');
+//  homeSection.classList.add('hidden')
+// //  console.log(homeSection.classList)  
+// const playgroundSection = document.getElementById('play-ground'); 
+// playgroundSection.classList.remove('hidden');
+// // console.log(playgroundSection.classList)
+// }
+
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+}
