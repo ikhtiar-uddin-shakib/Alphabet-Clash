e05730a0fbf34aa23519a7387beb649ec73ab9cb
Md Ikhtiar Uddin <mdikhtiaruddin786@gmail.com>
2024-02-21 09:59:21 +0600

    remove bg by press

--- scripts/alpha.js
+++ scripts/alpha.js
@@ -7,8 +7,27 @@
 // // console.log(playgroundSection.classList)
 // }
 
-function handleKeyboardPress(){
-    console.log('Button Pressed');
+function handleKeyboardPress(event){
+    const playerPressed = event.key;
+    console.log("Player Pressed",playerPressed);
+    
+    //get the expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    console.log(playerPressed, expectedAlphabet)
+    
+
+    //check match or not 
+    if (playerPressed == expectedAlphabet){
+        console.log('you get a point');
+        console.log('You have pressed correctly', expectedAlphabet);
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+    }
+    else{
+        console.log('You missed, you lost a life');
+    }
 }
 //capture keyboard key press
 document.addEventListener('keyup', handleKeyboardPress)
@@ -17,7 +36,7 @@ document.addEventListener('keyup', handleKeyboardPress)
 function continueGame(){
     // step - 1: generate a random alphabet
     const alphabet = getARandomAlphabet();
-    console.log('Your random Alphabet ' + alphabet);
+    // console.log('Your random Alphabet ', alphabet);
 
     //set randomly generated alphabet to the screen (show)
     const currentAlphabetElement = document.getElementById('current-alphabet');
